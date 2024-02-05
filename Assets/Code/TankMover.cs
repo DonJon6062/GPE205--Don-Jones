ca6b1667f3011e0770bcc27065d018a52bcda51a
DonJon6062 <donjones@uat.edu>
2024-02-04 19:50:31 -0700

    GameManager created and movement enabled

---
+++ Assets/Code/TankMover.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMover : Mover
+{
+    // Variable to hold the Rigidbody Component
+    private Rigidbody rb;
+    //Variable to hold transform
+    private Transform tf;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        // Get the Rigidbody component
+        rb = GetComponent<Rigidbody>();
+        //get transform component
+        tf = GetComponent<Transform>();
+    }
+
+    public override void Move(Vector3 direction, float speed)
+    {
+        Vector3 moveVector = direction.normalized * speed * Time.deltaTime;
+        rb.MovePosition(rb.position + moveVector);
+    }
+    public override void Rotate(float turnSpeed)
+    {
+        tf.Rotate(0, turnSpeed*Time.deltaTime, 0);
+    }
+}
